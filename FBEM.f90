9df34ee351012d6f617f89c5d6504fe33c08ae0b
zshigrit <zshi.grit@gmail.com>
2021-10-03 20:42:15 -0700

    add hourly dynamic with old warming site weather data

--- FBEM.f90
+++ FBEM.f90
@@ -3,7 +3,8 @@ program main
         implicit none
       ! will use name list for key parameters
         real,parameter::Ox=0.21,Ca=410,Rgas=8.314 !Ca in ppm
-        real:: Ta=20.0,swc=0.35,I=1500.0,RH=0.6,LAI=5.0 !driving force;I: absorbed PAR
+        real:: Ta=20.0,swc=0.35,I=1500.0,RH=0.6,LAI=0.0 !driving force;I: absorbed PAR
+        real:: Rad
         real:: f_Ci,Ci,Tk,alpha_q
         real:: Vm_25,Ea_vm,Gamma_star25,Ea_gamma,Ea_Kc,Ea_Ko,Kc_25,Ko_25
         real:: r_JmVm
@@ -11,6 +12,8 @@ program main
         real:: Reco0,Q10,a1
         real:: Vm,Gamma_star,Kc,Ko,Jm,Jc,Je,A
         real:: es,D,Gs,An,Ac,Reco,NEE
+        character:: header
+        integer:: istat3,day,hour
         namelist /main_para/ f_Ci,alpha_q,Vm_25,Ea_vm,Gamma_star25,Ea_gamma,Ea_Kc, &
                              Ea_Ko,Kc_25,Ko_25,r_JmVm,g1,D0,kn,Reco0,Q10,a1
 
@@ -18,6 +21,25 @@ program main
         read(11,nml=main_para)
         close(11)
         
+        open(12,file='tair_rh_rad_hourly.in')!,status='old',access ='sequential',form='formatted')
+        open(13,file='df_swc_daily.in')
+        open(14,file='df_lai_daily.in')
+        !read(12,*) header
+        read(13,*) header
+        read(14,*) header
+        
+        open(15,file='cflux.out',status='new')
+        write(15,*)'An,Ac,Reco,NEE'
+        
+        do day = 1, 365
+        read(13,*,IOSTAT=istat3)swc
+        read(14,*,IOSTAT=istat3)LAI 
+        if(istat3<0)exit
+        
+        do hour = 1, 24
+        read(12,*,IOSTAT=istat3)Ta,RH,Rad
+        RH=RH/100.0
+        I = Rad*2.0
         Ci = f_Ci * Ca
         Tk = Ta+273.15
         Vm = Vm_25 * Arrhenius(Ea_vm,Rgas,Tk)
@@ -29,6 +51,7 @@ program main
         Jc = Vm*(Ci-Gamma_star)/(Ci+Kc*(1.+Ox/Ko))
         Je = (alpha_q*I*Jm/(sqrt(Jm**2+alpha_q**2*I**2)))*((Ci-Gamma_star)/(4*(Ci+2*Gamma_star)))  
         A = min(Jc,Je)
+        !funeJ=alpha*Qaparx*eJmxT/(alpha*Qaparx+2.1*eJmxT) in TECO
         
         es = exp(21.382-5347.5/Tk)
         D = 0.1*es*(1-RH)
@@ -40,11 +63,20 @@ program main
         Reco = Reco0*Q10**(Ta/10)*(swc/(swc+a1)) ! Ta in degree C;a1:moisture coefficient at which respiration is half the maximum
         NEE = Reco-Ac
         
+        write(15,*)An,',',Ac,',',Reco,',',NEE
         print*,'Vm=',Vm, 'A=',A
         print*,'Gs=',Gs, 'D=',D
         print*,'Jc=',Jc,'Je=',Je
         print*,'Reco=',Reco,'Ac=',Ac
         print*,'An=',An,'NEE= ',NEE
+        
+        end do
+        end do
+        
+        close(12)
+        close(13)
+        close(14)
+        close(15)
     contains
 
 !      function Jc(Vmx,Ci,Gamma_star,Kc,Ox,Ko)
